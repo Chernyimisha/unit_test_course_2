910079896f1a34cc1b6cdcf494d5c3ff1358fefa
Proshkin Michail <chernyimisha@rambler.ru>
2023-11-23 19:45:51 +0300

    save

---
+++ DZ4/book/BookRepository.java
@@ -0,0 +1,8 @@
+package DZ4.book;
+
+import java.util.List;
+
+public interface BookRepository {
+    Book findById(String id);
+    List<Book> findAll();
+}
---
+++ test/BookServiceTest.java
@@ -0,0 +1,29 @@
+import DZ4.book.Book;
+import DZ4.book.BookRepository;
+import DZ4.book.BookService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+public class BookServiceTest {
+
+    BookRepository bookRepository;
+
+    @BeforeEach
+    void setUp() {
+
+
+
+    }
+
+    @Test
+    void findBookByIdTest(){
+
+
+
+    }
+
+
+
+}
